8c1f054745d39423c59a66380362e073a3516eba
Carsten Götzinger <carsten.goetzinger@concedro.com>
2022-02-24 09:20:07 +0100

    Fix paths

--- src/Resources/contao/config/autoload.php
+++ src/Resources/contao/config/autoload.php
@@ -16,12 +16,12 @@
 ClassLoader::addClasses(array
 (
     // Classes
-    'Contao\CalendarExport' => 'vendor/craffft/contao-calendar-ical-bundle/src/Resources/contao/classes/CalendarExport.php',
-    'Contao\CalendarImport' => 'vendor/craffft/contao-calendar-ical-bundle/src/Resources/contao/classes/CalendarImport.php',
-    'Contao\ContentICal'    => 'vendor/craffft/contao-calendar-ical-bundle/src/Resources/contao/classes/ContentICal.php',
-    'Contao\Csv'            => 'vendor/craffft/contao-calendar-ical-bundle/src/Resources/contao/classes/Csv.php',
-    'Contao\CsvParser'      => 'vendor/craffft/contao-calendar-ical-bundle/src/Resources/contao/classes/CsvParser.php',
-    'Contao\CsvReader'      => 'vendor/craffft/contao-calendar-ical-bundle/src/Resources/contao/classes/CsvReader.php',
+    'Contao\CalendarExport' => 'vendor/cgoit/contao-calendar-ical-php8-bundle/src/Resources/contao/classes/CalendarExport.php',
+    'Contao\CalendarImport' => 'vendor/cgoit/contao-calendar-ical-php8-bundle/src/Resources/contao/classes/CalendarImport.php',
+    'Contao\ContentICal'    => 'vendor/cgoit/contao-calendar-ical-php8-bundle/src/Resources/contao/classes/ContentICal.php',
+    'Contao\Csv'            => 'vendor/cgoit/contao-calendar-ical-php8-bundle/src/Resources/contao/classes/Csv.php',
+    'Contao\CsvParser'      => 'vendor/cgoit/contao-calendar-ical-php8-bundle/src/Resources/contao/classes/CsvParser.php',
+    'Contao\CsvReader'      => 'vendor/cgoit/contao-calendar-ical-php8-bundle/src/Resources/contao/classes/CsvReader.php',
 ));
 
 /**
@@ -29,8 +29,8 @@ ClassLoader::addClasses(array
  */
 TemplateLoader::addFiles(array
 (
-    'be_import_calendar'              => 'vendor/craffft/contao-calendar-ical-bundle/src/Resources/contao/templates',
-    'be_import_calendar_confirmation' => 'vendor/craffft/contao-calendar-ical-bundle/src/Resources/contao/templates',
-    'be_import_calendar_csv_headers'  => 'vendor/craffft/contao-calendar-ical-bundle/src/Resources/contao/templates',
-    'ce_ical'                         => 'vendor/craffft/contao-calendar-ical-bundle/src/Resources/contao/templates',
+    'be_import_calendar'              => 'vendor/cgoit/contao-calendar-ical-php8-bundle/src/Resources/contao/templates',
+    'be_import_calendar_confirmation' => 'vendor/cgoit/contao-calendar-ical-php8-bundle/src/Resources/contao/templates',
+    'be_import_calendar_csv_headers'  => 'vendor/cgoit/contao-calendar-ical-php8-bundle/src/Resources/contao/templates',
+    'ce_ical'                         => 'vendor/cgoit/contao-calendar-ical-php8-bundle/src/Resources/contao/templates',
 ));
